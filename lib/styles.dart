50fe08ca2e316f9216b8dd7725acd629985501e5
Martin <marty.dampier@gmail.com>
2024-02-07 23:15:05 -0600

    Fooling around

---
+++ lib/styles.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class Styles{
+
+  static ButtonStyle TextButtonStyle = ButtonStyle(
+    backgroundColor: MaterialStateProperty.resolveWith((states) => null),
+    foregroundColor: MaterialStateProperty.resolveWith((states) => null),
+    shape: MaterialStateProperty.resolveWith((states) => null)
+  );
+}
